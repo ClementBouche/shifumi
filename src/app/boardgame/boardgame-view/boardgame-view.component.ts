77e8c2007adcde6ae3336e4ec2b484788a02b809
Clément Bouché <clement.bouche@ign.fr>
2019-03-05 17:13:35 +0100

    rollback to previous view

--- src/app/boardgame/boardgame-view/boardgame-view.component.ts
+++ src/app/boardgame/boardgame-view/boardgame-view.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 import { ActivatedRoute, ParamMap } from '@angular/router';
 import { switchMap } from 'rxjs/operators';
 
