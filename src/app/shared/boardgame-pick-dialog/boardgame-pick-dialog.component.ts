dc6dc70177bd24b5d99ba955eeaf809eb8e2a97f
ClementBouche <cbouche44@gmail.com>
2019-04-17 11:32:22 +0200

    ajout subscription pour unscribe on destroy

--- src/app/shared/boardgame-pick-dialog/boardgame-pick-dialog.component.ts
+++ src/app/shared/boardgame-pick-dialog/boardgame-pick-dialog.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, Inject, ChangeDetectorRef } from '@angular/core';
+import { Component, OnInit, Inject, ChangeDetectorRef, OnDestroy } from '@angular/core';
 import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { FormControl } from '@angular/forms';
-import { Observable } from 'rxjs';
-import { map, startWith, debounceTime } from 'rxjs/operators';
+import { Subscription } from 'rxjs';
+import { debounceTime } from 'rxjs/operators';
 
 import { Boardgame } from './../../boardgame/shared/model/boardgame.model';
 import { BoardgameSearch } from './../../boardgame/shared/model/boardgame-search.model';
@@ -13,11 +13,13 @@ import { BoardgameService } from './../../boardgame/shared/services/boardgame.se
   templateUrl: './boardgame-pick-dialog.component.html',
   styleUrls: ['./boardgame-pick-dialog.component.css']
 })
-export class BoardgamePickDialogComponent implements OnInit {
+export class BoardgamePickDialogComponent implements OnInit, OnDestroy {
 
   boardgameForm: FormControl = new FormControl();
 
   boargames: Boardgame[];
+
+  private formSubscription: Subscription;
 
   constructor(
     private dialogRef: MatDialogRef<BoardgamePickDialogComponent>,
@@ -27,7 +29,7 @@ export class BoardgamePickDialogComponent implements OnInit {
   ) { }
 
   ngOnInit() {
-    this.boardgameForm.valueChanges
+    this.formSubscription = this.boardgameForm.valueChanges
         .pipe(debounceTime(500))
         .subscribe((namePart) => {
           const search = new BoardgameSearch().deserialize({name: namePart, size: 10});
@@ -36,6 +38,12 @@ export class BoardgamePickDialogComponent implements OnInit {
             this.cd.markForCheck();
           });
         });
+  }
+
+  ngOnDestroy() {
+    if (this.formSubscription) {
+      this.formSubscription.unsubscribe();
+    }
   }
 
   onNoClick(): void {
