1521cc6c3ea2d07495653dbac220a6c1fdfacb93
ClementBouche <cbouche44@gmail.com>
2019-05-07 16:51:03 +0200

    minor fixed to obsolete call

--- src/app/play/shared/components/play-list/play-list.component.ts
+++ src/app/play/shared/components/play-list/play-list.component.ts
@@ -1,10 +1,7 @@
 import { Component, OnInit, ChangeDetectorRef, Input } from '@angular/core';
 import { Router } from '@angular/router';
 
-import { Boardgame } from 'src/app/boardgame/shared/model/boardgame.model';
-
 import { Play } from '../../model/play.model';
-import { PlayService } from '../../services/play.service';
 
 @Component({
   selector: 'app-play-list',
@@ -13,26 +10,14 @@ import { PlayService } from '../../services/play.service';
 })
 export class PlayListComponent implements OnInit {
 
-  @Input() boardgame: Boardgame;
-
   @Input() plays: Play[];
 
   constructor(
     private router: Router,
-    private playService: PlayService,
     private cd: ChangeDetectorRef
   ) { }
 
   ngOnInit() {
-    // if input
-    if (this.boardgame) {
-      this.playService.getBoardgamePlays(this.boardgame).then((result) => {
-        this.plays = result;
-        this.cd.markForCheck();
-      });
-    } else {
-      // ras
-    }
   }
 
   view(playid: string) {
