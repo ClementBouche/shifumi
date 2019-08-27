78aa20c9939d75f2f929bc6ad573a1445f18b89c
unknown <cbouche44@gmail.com>
2019-08-27 23:59:53 +0200

    module(Layout): move layout components to new layoutModule

---
+++ src/app/shared-layout/components/layout/layout.component.ts
@@ -0,0 +1,45 @@
+import { Component, OnInit, AfterContentInit, ContentChildren, QueryList, Input } from '@angular/core';
+import { LayoutNavComponent } from '../layout-nav/layout-nav.component';
+import { LayoutSideComponent } from '../layout-side/layout-side.component';
+
+@Component({
+  selector: 'app-custom-layout',
+  template: `
+    <div class="layout" [style.padding]="padding">
+      <div class="nav">
+        <ng-content select="app-layout-nav"></ng-content>
+      </div>
+      <div class="content">
+        <ng-content></ng-content>
+      </div>
+      <div class="side">
+        <ng-content select="app-layout-side"></ng-content>
+      </div>
+      <div class="mobile">
+        <ng-content select="app-layout-mobile"></ng-content>
+      </div>
+    </div>
+  `,
+  styleUrls: ['./layout.component.css']
+})
+export class LayoutComponent implements OnInit, AfterContentInit {
+
+  @Input() padding: string = '30px 24px 0px 24px';
+
+  // nav component
+  @ContentChildren(LayoutNavComponent) navs: QueryList<LayoutNavComponent>;
+
+  // side component
+  @ContentChildren(LayoutSideComponent) sides: QueryList<LayoutSideComponent>;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+  ngAfterContentInit() {
+    // this.navs.forEach((instance) => {});
+    // this.sides.forEach((instance) => {});
+  }
+
+}
