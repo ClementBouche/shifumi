4deec04c81f651f217d6a2bcf693f60e764b3a32
ClementBouche <cbouche44@gmail.com>
2019-09-24 14:43:25 +0200

    passage de la note sur une echelle à 10

--- src/app/shared/components/input-rate/input-rate.component.ts
+++ src/app/shared/components/input-rate/input-rate.component.ts
@@ -19,13 +19,27 @@ export class InputRateComponent implements OnInit {
   }
 
   setRate(value: number) {
-    this.rate = value;
+    if (value === this.rate) {
+      this.rate = value - 1;
+    } else if (this.rate === value - 1) {
+      this.rate = null;
+    } else {
+      this.rate = value;
+    }
     this.rateChanged.emit(this.rate);
   }
 
   getRateColor(seuil: number) {
     const color = this.rate >= seuil ? 'accent' : '';
     return color;
+  }
+
+  getTooltip() {
+    return this.rate ? 'Note : ' + this.rate : 'Pas de note';
+  }
+
+  getIcon(seuil: number) {
+    return this.rate >= seuil ? 'star' : this.rate + 1 === seuil ? 'star_half' : 'star_border';
   }
 
 }
